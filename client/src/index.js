b0d1150c69dd2e210d037ad01ceac6f29cada8e8
david-aihe <doa9200@gmail.com>
2021-12-13 15:02:21 -0500

    added movie list to home

--- client/src/index.js
+++ client/src/index.js
@@ -3,8 +3,12 @@ import ReactDOM from 'react-dom';
 
 import './index.css';
 import App from './App';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 // You'll need to wrap <App /> for routing to work
 ReactDOM.render(
-    <App />, document.getElementById('root')
+    <Router>
+        <App />
+    </Router>
+    , document.getElementById('root')
 );
